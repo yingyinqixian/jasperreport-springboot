b9dd9b37999f0517d132add08fdcb4a5732738c6
yingyinqi <710441971@qq.com>
2020-06-05 22:26:20 +0800

    第一次提交

---
+++ readme.md
@@ -0,0 +1,12 @@
+###自定义报表jasperreport
+写了一个简单的自定义报表工具
+
+1，接口支持导出excel、word、html、图片、pdf格式
+
+2，接口支持打印功能
+
+3，用到了jasperreport的api以及常用的java设计模式，采用springboot快速构建
+
+3，如有问题联系方式：710441971@qq.com
+
+4，里面有些功能还不完善，暂时更新这些代码了
---
+++ src/main/java/com/yingyinqi/jasperreport/enums/ImageType.java
@@ -0,0 +1,15 @@
+package com.yingyinqi.jasperreport.enums;
+
+/**
+ * @author: create by libin
+ * @version: v1.0
+ * @description: com.yingyinqi.jasperreport.enums
+ * @date:2020/4/15
+ */
+public enum ImageType {
+    bmp,
+    png,
+    jpeg,
+    tif,
+    gif
+}
